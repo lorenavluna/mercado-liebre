4e2ecfc14338b4a82df5f0484b643d48af1512b1
lorenavluna <lorenavluna@gmail.com>
2021-05-18 22:20:17 -0300

    heroku

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const publicpath = path.resolve(__dirname, './public');
 app.use(express.static(publicpath));
 
-app.listen(3020, () => {
+app.listen(process.env.port||3000, () => {
     console.log('Servidor corriendo en el puerto 3000')
 });
 
