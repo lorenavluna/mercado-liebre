f69824707c9aa8022a12919b9941b6f8b7e61cc9
lorenavluna <lorenavluna@gmail.com>
2021-05-18 22:40:58 -0300

    heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.port||3000;
+const port = process.env.PORT || 3020;
 const publicpath = path.resolve(__dirname, './public');
 app.use(express.static(publicpath));
 
